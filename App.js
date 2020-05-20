076a9c1efda7f3ba3dd969437006a8b22e7d9c7e
cuongphung09 <43229377+cuongphung09@users.noreply.github.com>
2020-05-20 23:02:14 +0700

    20/05/2020

--- App.js
+++ App.js
@@ -1,65 +1,188 @@
-import React from 'react';
-import { Avatar } from 'react-native-elements';
-import { StyleSheet, Text, View } from 'react-native';
-import BrowseStackNavigation from "./Screen/Browse/BrowseStackNavigation"
-import HomeStackNavigation from "./Screen/Home/HomeStackNavigation"
-import DownloadStackNavigation from "./Screen/Download/DownloadStackNavigation"
-import SearchScreen from "./Screen/Search/SearchScreen"
-import ProfileScreen from "./Screen/ProfileScreen"
-import 'react-native-gesture-handler';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from "react";
+import { Avatar } from "react-native-elements";
+import { StyleSheet, Text, View, Button, UIManager,findNodeHandle } from "react-native";
+import SearchScreen from "./Screen/SearchScreen";
+import ProfileScreen from "./Screen/ProfileScreen";
+import HomeScreen from "./Screen/HomeScreen";
+import DownloadScreen from "./Screen/DownloadScreen";
+import BrowseScreen from "./Screen/BrowseScreen";
+import "react-native-gesture-handler";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+// const Tab = createBottomTabNavigator();
+import { NavigationContainer } from "@react-navigation/native";
+import { ScreenStack } from "react-native-screens";
+import { createStackNavigator } from "@react-navigation/stack";
+import Header from "./Component/header";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Alert } from "react-native";
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-import { NavigationContainer } from '@react-navigation/native';
-import { ScreenStack } from 'react-native-screens';
-import { createStackNavigator } from '@react-navigation/stack';
-const mainStack = createStackNavigator();
+function Home({ navigation }) {
+  return (
+    <Tab.Navigator initialRouteName="HomeScreen">
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="home-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="DownloadScreen"
+        component={DownloadScreen}
+        options={{
+          tabBarLabel: "Download",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="download" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="BrowseScreen"
+        component={BrowseScreen}
+        options={{
+          tabBarLabel: "Browse",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="grid" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="BrowseScreen"
+      <Stack.Navigator
+        headerMode="screen"
+        screenOptions={({ navigation, route }) => ({
+          headerTitleStyle: {
+            color: "white",
+          },
+          headerStyle: {
+            backgroundColor: "#212121",
+          },
+          headerRight: () => (
+            // <Button title="PRO" onPress={()=>navigation.navigate('Profile')}></Button>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Avatar
+                containerStyle={{ marginRight: 10 }}
+                size={25}
+                rounded
+                source={{
+                  uri:
+                    "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+                }}
+                onPress={() => navigation.navigate("Profile")}
+              />
+              <TouchableOpacity
+                onPress={() => {
+                  // UIManager.showPopupMenu(
+                  //   findNodeHandle(<Text>ABC</Text>),
+                  //   this.props.actions,
+                  //   this.onError,
+                  //   this.props.onPress
+                  // );
+                  Alert.alert("Ai rảnh ")
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="dots-vertical"
+                  color="white"
+                  size={25}
+                  style={{ marginRight: 10 }}
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+        })}
       >
-        <Tab.Screen name="HomeStackNavigation" component={HomeStackNavigation}
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
           options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home-outline" color={color} size={size} />
-            ),
-          }} />
-        <Tab.Screen name="DownloadScreen" component={DownloadStackNavigation}
-          options={{
-            tabBarLabel: 'Download',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="download" color={color} size={size} />
-            ),
-          }} />
-        <Tab.Screen name="BrowseScreen" component={BrowseStackNavigation}
-          options={{
-            tabBarLabel: 'Browse',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="grid" color={color} size={size} />
-            ),
-          }} />
-        <Tab.Screen name="Search" component={SearchScreen}
-          options={{
-            tabBarLabel: 'Search',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="magnify" color={color} size={size} />
-            ),
+            headerRightContainerStyle: {
+              opacity: 0,
+            },
           }}
         />
-        
-      </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator initialRouteName="BrowseScreen"
+//       >
+//         <Tab.Screen name="HomeStackNavigation" component={HomeStackNavigation}
+//           options={{
+//             tabBarLabel: 'Home',
+//             tabBarIcon: ({ color, size }) => (
+//               <MaterialCommunityIcons name="home-outline" color={color} size={size} />
+//             ),
+//           }} />
+//         <Tab.Screen name="DownloadScreen" component={DownloadStackNavigation}
+//           options={{
+//             tabBarLabel: 'Download',
+//             tabBarIcon: ({ color, size }) => (
+//               <MaterialCommunityIcons name="download" color={color} size={size} />
+//             ),
+//           }} />
+//         <Tab.Screen name="BrowseScreen" component={BrowseStackNavigation}
+//           options={{
+//             tabBarLabel: 'Browse',
+//             tabBarIcon: ({ color, size }) => (
+//               <MaterialCommunityIcons name="grid" color={color} size={size} />
+//             ),
+//           }} />
+//         <Tab.Screen name="Search" component={SearchScreen}
+//           options={{
+//             tabBarLabel: 'Search',
+//             tabBarIcon: ({ color, size }) => (
+//               <MaterialCommunityIcons name="magnify" color={color} size={size} />
+//             ),
+//           }}
+//         />
+
+//       </Tab.Navigator>
+//     </NavigationContainer>
+
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- Component/header.js
+++ Component/header.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 export default function Header({ navigation }) {
-    
+    console.log(navigation)
     return (
         
         <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
@@ -15,9 +15,7 @@ export default function Header({ navigation }) {
                     uri:
                         'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
                 }}
-                onPress={
-                    navigation.navigate('Profile')
-                }
+                onPress={()=>navigation.navigate('Profile')}
             />
             <MaterialCommunityIcons
                 name="dots-vertical"
